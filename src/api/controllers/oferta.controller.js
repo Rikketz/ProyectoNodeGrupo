f2a96af871cb96c71cdc9b94a5e2dbfc35fa53cd
Rikketz <jesussavi88@gmail.com>
2023-10-12 19:41:10 +0200

    hey

--- src/api/controllers/oferta.controller.js
+++ src/api/controllers/oferta.controller.js
@@ -1,17 +1,13 @@
 
 const Oferta = require("../models/oferta.model")
-
 const addOferta = async (req, res) => {
     try {
         const newOferta = new Oferta(req.body);
         const createdOferta = await newOferta.save();
         return res.status(200).json({ message: "proyecto creado", data: createdOferta })
-
     } catch (error) {
-
     }
 }
-
 const getOfertas = async (req, res) => {
     try {
         const ofertas = await Oferta.find().populate("inscritos")
@@ -20,7 +16,6 @@ const getOfertas = async (req, res) => {
         return res.json(error)
     }
 }
-
 const getOfertaById = async (req, res) => {
     try {
         const oferta = await Oferta.findById(req.params.id).populate("inscritos");
@@ -28,8 +23,19 @@ const getOfertaById = async (req, res) => {
     } catch (error) {
 
     }
-
 }
-
-module.exports = { addOferta, getOfertas, getOfertaById }
+const putOferta = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const putOferta = new Oferta(req.body);
+        putOferta._id = id;
+        const updatedOferta = await Oferta.findByIdAndUpdate(id, putOferta, {
+            new:true,
+        })
+        return res.status(200).json(updatedOferta);
+    } catch (error) {
+        return res.status(500).json(error)
+    }
+}
+module.exports = { addOferta, getOfertas, getOfertaById, putOferta }
 

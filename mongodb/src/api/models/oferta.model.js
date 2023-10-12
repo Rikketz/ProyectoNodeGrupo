3023bc6787bdec1305f6b768448c22daf9f97e20
Rikketz <jesussavi88@gmail.com>
2023-10-12 12:02:22 +0200

    modificacion de project a oferta

--- mongodb/src/api/models/oferta.model.js
+++ mongodb/src/api/models/oferta.model.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const projectSchema = new Schema({
+const ofertaSchema = new Schema({
     name: { type: String, require: true },
     description: { type: String, require: true },
     student: [{ type: Schema.ObjectId, ref: "student" }]
 }, {
-    collection: "project"
+    collection: "oferta"
 });
 //string, number, array, date, mixed, boolean,  ObjectId
 
-const Project = mongoose.model("project", projectSchema)
-module.exports = Project;
+const Oferta = mongoose.model("oferta", ofertaSchema)
+module.exports = Oferta;
--- mongodb/src/api/routes/project.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router();
-const { addProject, getProjects, getProjectById } = require("../controllers/project.controller")
-
-router.post("/", addProject)
-router.get("/", getProjects)
-router.get("/:id", getProjectById)
-
-module.exports = router;

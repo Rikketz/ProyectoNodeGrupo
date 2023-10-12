fd6eb4123edafcbe8e04bc0b82d47a392fc531ee
Javier Sanchez <javi.sm.77@gmail.com>
2023-10-12 12:18:38 +0200

    cambio archivos

---
+++ src/api/models/solicitante.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+
+const solicitanteSchema = new Schema({
+    name: { type: String, require: true },
+    username: { type: String, require: true },
+    age: { type: Number },
+    image: { type: String, default: "" },
+}, {
+    collection: "solicitante"
+});
+//string, number, array, date, mixed, boolean,  ObjectId
+
+const Solicitante = mongoose.model("student", solicitanteSchema)
+module.exports = Solicitante;
---
+++ src/api/routes/solicitante.routes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const { newSolicitante, getSolicitante, updateSolicitante, deleteSolicitante, getSolicitantebyName } = require("../controllers/solicitante.controller")
+
+const router = express.Router()
+const upload = require("../../middleware/upload.file");
+
+router.post("/register", upload.single("image"), newSolicitante) // crear
+//
+router.get("/", getSolicitante);
+router.get("/:nameSolicitante", getSolicitantebyName);
+router.put("/:id", updateSolicitante);
+router.delete("/:id", deleteSolicitante)
+
+
+
+module.exports = router;

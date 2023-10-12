d5f3cdb98db3aecb534dbd49a0c2f46e22784d45
Javier Sanchez <javi.sm.77@gmail.com>
2023-10-12 19:32:00 +0200

    añadimos funciones a user

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -4,12 +4,14 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-    user: {type: String, required: true},
+    name: {type: String, required: true},
+    lastname: {type: String, required: true},
+    phone: {type: Number, require: true},
     email: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, default: "user", enum: ["admin", "user", "company"] },
-
-}, {
+},
+{
     collection: "user", timestamps: true
 });
 //string, number, array, date, mixed, boolean,  ObjectId
--- src/api/routes/user.routes.js
+++ src/api/routes/user.routes.js
@@ -1,14 +1,16 @@
 const express = require("express")
 
-const { register, login, profile, getUser, getUserById } = require("../controllers/user.controller")
+const { register, login, profile, getUser, getUserById, getUserByEmail, putUser, deleteUser } = require("../controllers/user.controller")
 const { isAuth } = require("../../middleware/auth")
 const router = express.Router()
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/profile", [isAuth], profile); // recibir un token
-router.get("/:id", getUserById)
-router.get("/", getUser)
-
+router.get("/:id", getUserById);
+router.get("/", getUser);
+router.get("/:email", getUserByEmail);
+router.put("/:id", putUser);
+router.delete("/:id", deleteUser)
 
 module.exports = router;
